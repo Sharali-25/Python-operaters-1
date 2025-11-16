485fd22a5d58af3028c0979cd88a88463387de3b
Sharali-25 <Sharaliarkansas@gmail.com>
2025-11-16 09:26:50 -0600

    Add files via upload

---
+++ Actvity3.py
@@ -0,0 +1,8 @@
+num=int(input("Plz enter a number: "))
+if num<15:
+    print("num is smaller than 15")
+    print("iam an if block")
+else:
+    print("num is greater than 15")
+    print("iam in else block")
+print("iam not in if and not in else block")
