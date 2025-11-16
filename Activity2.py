0a1379bad9e2b9ba120fa1311073d9ea28406404
Sharali-25 <Sharaliarkansas@gmail.com>
2025-11-16 09:16:31 -0600

    Add files via upload

--- Activity2.py
+++ Activity2.py
@@ -1,7 +1,7 @@
-Amount=int(input("Plz enter amount for withdraw"))
-note_1=Amount//100
-note_2=(Amount%100)//50
-note_3=((Amount%100)%50)//10
-print("Note of 100 rupee",note_1)
-print("Note of 50 rupee",note_2)
-print("Note of 10 rupee",note_3)
+actual_cost=float(input("plz enter the actual product price: "))
+sell_cost=float(input("plz enter the sell amount: "))
+if sell_cost>actual_cost:
+    amount=sell_cost-actual_cost
+    print("total profit={0}".format(amount))
+else:
+    print("no profit")
